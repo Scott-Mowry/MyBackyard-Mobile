341229faf8746799590a3d06c35a4f16d728a732
David Ordine <david.ordine@grad.ufsc.br>
2025-09-02 23:23:35 -0300

    authentication updates

--- lib/legacy/Arguments/content_argument.dart
+++
@@ -1,5 +0,0 @@
-class ContentRoutingArgument {
-  String title, contentType, url;
-  Function(bool)? isMerchantSetupDone;
-  ContentRoutingArgument({required this.title, required this.contentType, required this.url, this.isMerchantSetupDone});
-}
--- lib/legacy/Arguments/profile_screen_arguments.dart
+++
@@ -1,8 +0,0 @@
-import 'package:backyard/core/model/user_profile_model.dart';
-
-class ProfileScreenArguments {
-  bool? isMe, isUser;
-  UserProfileModel? user;
-  bool? isBusinessProfile;
-  ProfileScreenArguments({this.isBusinessProfile, this.isMe, this.isUser, this.user});
-}
